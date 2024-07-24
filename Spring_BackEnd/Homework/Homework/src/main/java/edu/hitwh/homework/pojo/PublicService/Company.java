04bad94f388d6b6195406729df84436d661c6b07
Hui_Loading <3080811164@qq.com>
2024-07-24 10:19:11 +0800

    补充项目的启动项和响应结果实体类

--- Spring_BackEnd/Homework/Homework/src/main/java/edu/hitwh/homework/pojo/PublicService/Company.java
+++ Spring_BackEnd/Homework/Homework/src/main/java/edu/hitwh/homework/pojo/PublicService/Company.java
@@ -1,6 +1,5 @@
 package edu.hitwh.homework.pojo.PublicService;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Company {
-
+    private int companyId; // 单位编码
+    private String companyName; // 单位名称
+    private String companyType; // 单位类型 (0, 1, 2, 3)
+    private String address; // 地址
+    private String postcode; // 邮编
+    private String phoneNumber; // 联系电话
 }
